2cd35f0bcb7f7c3482e97894f960c289656711de
nnrmina <narminaibrahimova2@gmail.com>
2025-02-13 08:10:15 -0800

    Narmina

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+@app.route("/narmina")
+def narmina():
+    return "l m so perfect"
 
 if __name__ == "__main__":
     app.run(debug=True)
